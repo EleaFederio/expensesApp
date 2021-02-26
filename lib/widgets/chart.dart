9b0ec9bfbc2e8fa6d7429b2c9683f096a899d1eb
EleazarFederio <eleazarfederio_@outlook.com>
2021-02-26 22:58:32 +0800

    resolve error if empty

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -11,13 +11,13 @@ class Chart extends StatelessWidget {
     return List.generate(7, (index) {
       final weekday = DateTime.now().subtract(Duration(days: index));
 
-      double totalSum = 0.0;
+      var totalSum = 0.0;
       for (var i = 0; i < recentTransactions.length; i++) {
         // print(i.toString() + ' ::: ' + recentTransactions[i].date.day);
         if (recentTransactions[i].date.day == weekday.day &&
             recentTransactions[i].date.month == weekday.month &&
             recentTransactions[i].date.year == weekday.year) {
-          totalSum = recentTransactions[i].amount;
+          totalSum += recentTransactions[i].amount;
         }
       }
 
@@ -30,21 +30,33 @@ class Chart extends StatelessWidget {
 
   double get totalSpending {
     return groupedTransactionValues.fold(0.0, (sum, item) {
+      print(sum);
       return sum + item['amount'];
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    print('+++++++++' + groupedTransactionValues.toString());
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(
-        children: groupedTransactionValues.map((data) {
-          return ChartBar(data['day'], data['amount'],
-              (data['amount'] as double) / totalSpending);
-        }).toList(),
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactionValues.map((data) {
+            print('+++++++++' + data['amount'].toString());
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(
+                  data['day'],
+                  data['amount'],
+                  totalSpending == 0.0
+                      ? 0.0
+                      : (data['amount'] as double) / totalSpending),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -12,7 +12,7 @@ class ChartBar extends StatelessWidget {
     // print('XXXXXXXXXXXXXXXXXXXXXXXXX' + spendingAmount.toString());
     return Column(
       children: <Widget>[
-        Text('₱ ${spendingAmount.toStringAsFixed(0)}'),
+        FittedBox(child: Text('₱ ${spendingAmount.toStringAsFixed(0)}')),
         SizedBox(
           height: 4,
         ),
